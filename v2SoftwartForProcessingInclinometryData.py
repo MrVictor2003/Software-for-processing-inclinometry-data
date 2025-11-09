62408717dcdd0cb33c682d60a09a3d92594f4aa9
MrVictor2003 <hhsvdjsjdbdvshsh@gmail.com>
2025-11-09 21:05:44 +0300

    добавлен класс для сбора сырых данных для 7 измерений

---
+++ v2SoftwartForProcessingInclinometryData.py
@@ -0,0 +1,41 @@
+import math
+import matplotlib.pyplot as plt
+
+class RawDataCollector:
+    "Класс для импорта файла с сырыми данными измерений"
+    def __init__(self, filename: str):
+        self.filename = filename
+        self.raw_data = []
+
+    def read_data(self):
+        with open(self.filename, 'r') as file:
+            for line in file:
+                values = line.strip().split(',')
+                if len(values) == 7:
+                    data_measure = {
+                        'Depth': float(values[0]),
+                        'x_accel': float(values[1]),
+                        'y_accel': float(values[2]),
+                        'z_accel': float(values[3]),
+                        'x_magn': float(values[4]),
+                        'y_magn': float(values[5]),
+                        'z_magn': float(values[6]),
+                    }
+
+                self.raw_data.append(data_measure)
+
+        return self.raw_data
+
+
+
+
+
+
+#тут написать класс для расчета для 7 измерений
+
+#тут написать класс для расчета для 10 измерений
+
+if __name__ == "__main__":
+    raw_data1 = RawDataCollector("./data/raw_data_ishodnie.txt")
+    raw_data1_lst = raw_data1.read_data()
+    print(raw_data1_lst)
