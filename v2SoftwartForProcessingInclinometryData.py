5b1cc0b80fe79b4e0745bff4491d621170986dcf
MrVictor2003 <hhsvdjsjdbdvshsh@gmail.com>
2025-11-09 22:23:29 +0300

    добавлен проверка формата файла в функции main

--- v2SoftwartForProcessingInclinometryData.py
+++ v2SoftwartForProcessingInclinometryData.py
@@ -1,7 +1,7 @@
 import math
 import matplotlib.pyplot as plt
 
-class RawDataCollector:
+class RawDataCollectorSevenMeasurements:
     "Класс для импорта файла с сырыми данными измерений"
     def __init__(self, filename: str):
         self.filename = filename
@@ -19,23 +19,57 @@ class RawDataCollector:
                         'z_accel': float(values[3]),
                         'x_magn': float(values[4]),
                         'y_magn': float(values[5]),
-                        'z_magn': float(values[6]),
+                        'z_magn': float(values[6])
                     }
 
                 self.raw_data.append(data_measure)
 
         return self.raw_data
 
+class RawDataCollectorTenMeasurements:
+    "Класс для импорта файла с сырыми данными измерений"
+    def __init__(self, filename: str):
+        self.filename = filename
+        self.raw_data = []
 
+    def read_data(self):
+        with open(self.filename, 'r') as file:
+            for line in file:
+                values = line.strip().split(',')
+                if len(values) == 10:
+                    data_measure = {
+                        'Depth': float(values[0]),
+                        'x_accel': float(values[1]),
+                        'y_accel': float(values[2]),
+                        'z_accel': float(values[3]),
+                        'x_gyro': float(values[4]),
+                        'y_gyro': float(values[5]),
+                        'z_gyro': float(values[6]),
+                        'x_magn': float(values[7]),
+                        'y_magn': float(values[8]),
+                        'z_magn': float(values[9])
+                    }
 
+                self.raw_data.append(data_measure)
 
-
+        return self.raw_data
 
 #тут написать класс для расчета для 7 измерений
 
 #тут написать класс для расчета для 10 измерений
 
 if __name__ == "__main__":
-    raw_data1 = RawDataCollector("./data/raw_data_ishodnie.txt")
-    raw_data1_lst = raw_data1.read_data()
-    print(raw_data1_lst)
+    filename = "./data/raw_data_ishodnie.txt"
+
+    file_format = 0
+
+    try:
+        raw_data = RawDataCollectorTenMeasurements(filename).read_data()
+        print("Формат 10 измерений в строке")
+    except:
+        try:
+            raw_data = RawDataCollectorSevenMeasurements(filename).read_data()
+            print("Формат 7 измерений в строке")
+        except:
+            ValueError ('Неправильный формат файла')
+    print(raw_data)
