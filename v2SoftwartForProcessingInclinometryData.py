3952d376f38b924b9fcc06dbf7e5ce4615616578
MrVictor2003 <hhsvdjsjdbdvshsh@gmail.com>
2025-11-09 23:57:09 +0300

    добавлен метод get_depth_azimuths_zangles класса ConveterRawDataToLenghtDangleZangle для формирования списка со всеми параметрами для расчета координат, формирующих траекторию скважины

--- v2SoftwartForProcessingInclinometryData.py
+++ v2SoftwartForProcessingInclinometryData.py
@@ -31,13 +31,14 @@ class ConverterRawDataToDepthDangleZangle:
         self.lst_raw_data = lst_raw_data
         self.lst_azimuths = []
         self.lst_zangles = []
-        self.lst_depth = []
+        self.lst_depths = []
+        self.lst_depth_azimuths_zangles = []
 
     def get_depth(self):
-        self.lst_depth = []
+        self.lst_depths = []
         for i in self.lst_raw_data:
-            self.lst_depth.append(i.get('depth'))
-        return self.lst_depth
+            self.lst_depths.append(i.get('depth'))
+        return self.lst_depths
 
     def calculate_azimuths(self):
         self.lst_azimuths = []
@@ -83,6 +84,62 @@ class ConverterRawDataToDepthDangleZangle:
 
         return self.lst_zangles
 
+    def get_depth_azimuths_zangles(self):
+        for i in range(len(self.lst_raw_data)):
+            data_depth_azimuths_zangles = {
+                'depth': float(self.lst_depths[i]),
+                'azimuth': float(self.lst_azimuths[i]),
+                'zangle': float(self.lst_zangles[i])
+            }
+            self.lst_depth_azimuths_zangles.append(data_depth_azimuths_zangles)
+
+        return self.lst_depth_azimuths_zangles
+
+# class ConverterToXYH:
+#     "Класс для расчета координат по расстояниям, дирекционным и зенитным углам"
+#     def __init__(self, lst_azimuths, lst_zangles, lst_depth, x=0, y=0, z=0):
+#         self.x = x
+#         self.y = y
+#         self.z = z
+#         self.lst_azimuths = lst_azimuths
+#         self.lst_zangles = lst_zangles
+#         self.lst_depth = lst_depth
+#         self.lst_x = []
+#         self.lst_y = []
+#         self.lst_z = []
+#
+#     def calculate_x(self):
+#         self.lst_x.append(self.x)
+#         for i in self.lst_lenght_dangle_zangle:
+#             self.x += i.get('lenght')*math.cos(i.get('dangle'))\
+#                               *math.cos(i.get('zangle'))
+#             self.lst_x.append(self.x)
+#
+#         return self.lst_x
+#
+#     def calculate_y(self):
+#         self.lst_y.append(self.y)
+#         for i in self.lst_lenght_dangle_zangle:
+#             self.y += i.get('lenght')*math.sin(i.get('dangle'))\
+#                               *math.cos(i.get('zangle'))
+#             self.lst_y.append(self.y)
+#
+#         return self.lst_y
+#
+#     def calculate_z(self):
+#         self.lst_z.append(self.z)
+#         for i in self.lst_lenght_dangle_zangle:
+#             self.z += -i.get('lenght') * math.sin(i.get('zangle'))
+#             self.lst_z.append(self.z)
+#         return self.lst_z
+#
+#     def get_coords(self):
+#         for i in range(len(self.lst_x)):
+#             self.lst_xyz.append({'x': self.lst_x[i],
+#                                  'y': self.lst_y[i],
+#                                  'z': self.lst_z[i]})
+#         return self.lst_xyz
+
 
 def main():
     #Чтение файла с сырыми данными
@@ -103,9 +160,13 @@ def main():
     print(lst_zangles)
     print(len(lst_zangles))
 
-    lst_depth = converter_raw_data.get_depth()
-    print(lst_depth)
-    print(len(lst_depth))
+    lst_depths = converter_raw_data.get_depth()
+    print(lst_depths)
+    print(len(lst_depths))
+
+    lst_de_az_za = converter_raw_data.get_depth_azimuths_zangles()
+    print(lst_de_az_za)
+    print(len(lst_de_az_za))
 
 if __name__ == "__main__":
     main()
