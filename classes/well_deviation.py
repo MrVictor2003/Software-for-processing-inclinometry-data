dc08d11671217ba398dc6a2a56088d939e07947d
MrVictor2003 <hhsvdjsjdbdvshsh@gmail.com>
2025-11-15 15:09:17 +0300

    исправлена длина строк в классе WellDeviation в соответствии с PEP-8

--- classes/well_deviation.py
+++ classes/well_deviation.py
@@ -12,17 +12,22 @@ class WellDeviation:
 
     def get_max_lateral_deviation(self):
         for i in self.lst_xyz:
-            self.lst_lateral_deviation.append(math.sqrt(i.get('x')**2 + i.get('y')**2))
+            self.lst_lateral_deviation.append(
+                math.sqrt(i.get('x')**2+i.get('y')**2))
 
-        self.index_max_lateral_deviation = self.lst_lateral_deviation.index(max(
-            self.lst_lateral_deviation))
+        self.index_max_lateral_deviation = (
+            self.lst_lateral_deviation.index(
+                max(self.lst_lateral_deviation)))
 
-        self.deviation_x = (self.lst_xyz[self.index_max_lateral_deviation].get('x') -
-                            self.lst_xyz[0].get('x'))
+        self.deviation_x = (
+                self.lst_xyz[self.index_max_lateral_deviation].get('x')
+                -self.lst_xyz[0].get('x'))
 
-        self.deviation_y = (self.lst_xyz[self.index_max_lateral_deviation].get('y') -
-                            self.lst_xyz[0].get('y'))
+        self.deviation_y = (
+                self.lst_xyz[self.index_max_lateral_deviation].get('y')
+                -self.lst_xyz[0].get('y'))
 
-        self.max_lateral_deviation = round(math.sqrt(self.deviation_x**2 + self.deviation_y**2),3)
+        self.max_lateral_deviation = round(
+            math.sqrt(self.deviation_x**2+self.deviation_y**2),3)
 
         return self.max_lateral_deviation
